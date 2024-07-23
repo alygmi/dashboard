faf89935fc001669b35d52fa08b4f92ee96005a3
alygmi <nurrusulialadiyat@gmail.com>
2024-07-23 13:35:39 +0700

    first commit

---
+++ react-admin-ui/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Aadmin</span>
+      <span>© Adit Dev Admin Dashboard nyontek dari lamadev</span>
+    </div>
+  );
+};
+
+export default Footer;
